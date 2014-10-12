ae375538487b8cd9e08a6a5af2dc5d5eb9061533
mitz <hayasaka.mitsuru@gmail.com>
2014-10-13 02:59:55 +0900

    tough-cookie-filestore

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var cookies = require('request/lib/cookies');
 var CookieJar = require('tough-cookie').CookieJar;
-var filestore = require('tough-cookie/lib/filestore').FileCookieStore;
+var filestore = require('tough-cookie-filestore/lib/filestore').FileCookieStore;
 
 var filestore;
 cookies.jar = function(store) {
